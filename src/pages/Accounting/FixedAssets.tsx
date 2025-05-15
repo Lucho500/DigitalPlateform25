e5a806030576a32d8e9f0c7bf6d90b72cbf1426d
Lucho500 <luisallauca50@gmail.com>
2025-05-15 15:31:32 +0200

    Create Fixed Assets page component

---
+++ src/pages/Accounting/FixedAssets.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function FixedAssets() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-semibold mb-6">Fixed Assets</h1>
+      <div className="bg-white rounded-lg shadow p-6">
+        <p className="text-gray-600">Fixed assets management content will be displayed here.</p>
+      </div>
+    </div>
+  );
+}
