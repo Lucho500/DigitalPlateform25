6c6890df39bb174c456b05dd64caa9dd807e404d
Lucho500 <luisallauca50@gmail.com>
2025-05-15 15:34:37 +0200

    Remove Accounting Section

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,6 @@ import { Declarations } from './pages/Declarations';
 import { Settings } from './pages/Settings';
 import { Payroll } from './pages/Payroll';
 import { Administrative } from './pages/Administrative';
-import { Accounting } from './pages/Accounting';
-import { Clients } from './pages/Accounting/Clients';
-import { Suppliers } from './pages/Accounting/Suppliers';
-import { BankReconciliation } from './pages/Accounting/BankReconciliation';
-import { InterimClosing } from './pages/Accounting/InterimClosing';
-import { AccountingAnalytics } from './pages/Accounting/Analytics';
-import { AnnualClosing } from './pages/Accounting/AnnualClosing';
-import { PayrollImport } from './pages/Accounting/PayrollImport';
-import { Inventory } from './pages/Accounting/Inventory';
-import { FixedAssets } from './pages/Accounting/FixedAssets';
-import { AuditSupport } from './pages/Accounting/AuditSupport';
 
 function App() {
   return (
@@ -36,19 +25,6 @@ function App() {
           <Route path="settings" element={<Settings />} />
           <Route path="payroll" element={<Payroll />} />
           <Route path="administrative" element={<Administrative />} />
-          <Route path="accounting" element={<Accounting />}>
-            <Route path="clients" element={<Clients />} />
-            <Route path="suppliers" element={<Suppliers />} />
-            <Route path="bank-reconciliation" element={<BankReconciliation />} />
-            <Route path="interim-closing" element={<InterimClosing />} />
-            <Route path="analytics" element={<AccountingAnalytics />} />
-            <Route path="annual-closing" element={<AnnualClosing />} />
-            <Route path="payroll-import" element={<PayrollImport />} />
-            <Route path="inventory" element={<Inventory />} />
-            <Route path="fixed-assets" element={<FixedAssets />} />
-            <Route path="audit-support" element={<AuditSupport />} />
-            <Route index element={<Navigate to="clients" replace />} />
-          </Route>
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
