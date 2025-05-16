598a7d7a359b09bc7b9d0915cfd3913b98f7a922
Lucho500 <luisallauca50@gmail.com>
2025-05-16 11:29:30 +0200

    Restore Finance component with all implementations

--- src/utils/formatters.ts
+++
@@ -1,8 +0,0 @@
-export const formatCurrency = (value: number) => {
-  return new Intl.NumberFormat('fr-FR', {
-    style: 'currency',
-    currency: 'EUR',
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0
-  }).format(value);
-};
